68015d01c71168b31b5b9584f94d83d33c0a5f55
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2021-03-18 22:13:26 +0530

    Fix action onclick

--- background.js
+++ background.js
@@ -12,17 +12,29 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // setting page according to status of user login
 
-chrome.action.onClicked.addListener(() => {
-  console.log("heyyy");
+
+
+function reddenPage() {
+  console.log("redden");
+  document.body.style.backgroundColor = 'red';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  console.log("in listener on click");
   chrome.storage.sync.get('id', ({id})=> {
-    console.log(id);
-    if (id) {
-      chrome.action.setPopup({popup: 'popup.html'});
-    } 
-    else {
-      console.log("ui is signup");
-      chrome.action.setPopup({popup: 'signup.html'});
-    }
-  
+  console.log(id);
+  if (id) {
+    chrome.action.setPopup({popup: 'popup.html'});
+  } 
+  else {
+    console.log("ui is signup");
+    chrome.action.setPopup({popup: 'signup.html'});
+  }
   });
-})
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: reddenPage
+  // });
+});
+
+
